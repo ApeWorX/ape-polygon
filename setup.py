8063d8070b554b9f61b6897ec1194197f7b395a2
Ninjagod1251 <15219900+Ninjagod1251@users.noreply.github.com>
2022-06-18 03:34:09 +0700

    feat: upgrade ape (#3)

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "importlib-metadata ; python_version<'3.8'",
-        "eth-ape>=0.2.1,<0.3.0",
+        "eth-ape>=0.3.0,<0.4.0",
     ],  # NOTE: Add 3rd party libraries here
     python_requires=">=3.7.2,<3.11",
     extras_require=extras_require,
