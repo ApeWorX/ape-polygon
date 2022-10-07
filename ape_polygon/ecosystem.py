e2327bbfca590078538283587f8f5c8f0ef4852b
unparalleled-js <yingthi@live.com>
2022-10-07 17:55:16 -0500

    chore: lint

--- ape_polygon/ecosystem.py
+++ ape_polygon/ecosystem.py
@@ -11,13 +11,19 @@ NETWORKS = {
 }
 
 
-def _create_network_config(required_confirmations: int = 1, block_time: int = 2, **kwargs) -> NetworkConfig:
+def _create_network_config(
+    required_confirmations: int = 1, block_time: int = 2, **kwargs
+) -> NetworkConfig:
     # Helper method to isolate `type: ignore` comment.s
-    return NetworkConfig(required_confirmations=required_confirmations, block_time=block_time, **kwargs)  # type: ignore
+    return NetworkConfig(
+        required_confirmations=required_confirmations, block_time=block_time, **kwargs
+    )  # type: ignore
 
 
 def _create_local_config(default_provider: Optional[str] = None) -> NetworkConfig:
-    return _create_network_config(required_confirmations=0, block_time=0, default_provider=default_provider)
+    return _create_network_config(
+        required_confirmations=0, block_time=0, default_provider=default_provider
+    )
 
 
 class PolygonConfig(PluginConfig):
