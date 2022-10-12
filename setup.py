487cb4bdf7eee45c92e9c7186b58f64de835bfa6
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2022-10-12 15:01:50 -0500

    chore: bump ape core 0.5.2

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     url="https://github.com/ApeWorX/ape-polygon",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.5.1,<0.6",
+        "eth-ape>=0.5.2,<0.6",
     ],
     python_requires=">=3.8,<3.11",
     extras_require=extras_require,
