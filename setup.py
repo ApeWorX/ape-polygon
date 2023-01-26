3e89a903020292886623b27d4e565be73f973156
unparalleled-js <yingthi@live.com>
2023-01-26 14:39:19 -0600

    chore: six

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import find_packages, setup  # type: ignore
+from setuptools import find_packages, setup
 
 extras_require = {
     "test": [  # `test` GitHub Action jobs uses this
@@ -10,10 +10,14 @@ extras_require = {
         "hypothesis>=6.2.0,<7",  # Strategy-based fuzzer
     ],
     "lint": [
-        "black>=22.6.0,<23",  # auto-formatter and linter
-        "mypy>=0.971,<1",  # Static type analyzer
-        "flake8>=4.0.1,<5",  # Style linter
-        "isort>=5.10.1,<6",  # Import sorting linter
+        "black>=22.12.0",  # auto-formatter and linter
+        "mypy>=0.991",  # Static type analyzer
+        "types-setuptools",  # Needed due to mypy typeshed
+        "flake8>=5.0.4",  # Style linter
+        "isort>=5.10.1",  # Import sorting linter
+        "mdformat>=0.7.16",  # Auto-formatter for markdown
+        "mdformat-gfm>=0.3.5",  # Needed for formatting GitHub-flavored markdown
+        "mdformat-frontmatter>=0.4.1",  # Needed for frontmatters-style headers in issue templates
     ],
     "release": [  # `release` GitHub Action job uses this
         "setuptools",  # Installation tool
@@ -53,7 +57,7 @@ setup(
     url="https://github.com/ApeWorX/ape-polygon",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.5.2,<0.6",
+        "eth-ape>=0.6.0,<0.7",
     ],
     python_requires=">=3.8,<3.11",
     extras_require=extras_require,
